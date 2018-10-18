d529d0eea8c627f7f4e132767fff6e3eb07deaf8
ernestofacal <42684331+ernestofacal@users.noreply.github.com>
2018-10-17 23:54:57 -0300

    modifico evaluarRecursiva , evaluarMsucesivas ,y obtencion del grado del polinomio

--- src/tp3/Main.java
+++ src/tp3/Main.java
@@ -3,9 +3,14 @@ package tp3;
 public class Main {
 
 	public static void main(String[] args) {
-		Polinomio miPolinomio = new Polinomio(4,new double[] {2, -6, 2, -1});
-		System.out.println(miPolinomio.evaluarHorner(4));
-		System.out.println(miPolinomio.evaluarMsucesivas(4));
+		Polinomio miPolinomio = new Polinomio(3,new double[] {4, 5, 1, 2});
+		System.out.println(miPolinomio.evaluarMsucesivas(2));
+		System.out.println(miPolinomio.evaluarRecursiva(2));
+		System.out.println(miPolinomio.evaluarProgDinamica(2));
+		System.out.println(miPolinomio.evaluarMejorada(2));		///progDinamica mejorada
+		System.out.println(miPolinomio.evaluarPow(2));
+		System.out.println(miPolinomio.evaluarHorner(2));
+		
 	}
 
 }
--- src/tp3/Polinomio.java
+++ src/tp3/Polinomio.java
@@ -16,15 +16,54 @@ public class Polinomio {
 	
 	
 	
-	public double evaluarMsucesivas(double x) {
+	public double evaluarMsucesivas(double x) {		///1	
+		 double contador=0;
+		 double aux=0;
+		 int decrementador=0;
+		
+		for (double d : this.coeficientes) {
+			contador=d;
+			for(int i=0;i<this.grado-decrementador;i++)
+			{
+				contador*=x;
+			}
+			decrementador++;
+			aux+=contador;
+			contador=0;
+		}
+
+		return aux;
+	}
+	
+	public double evaluarRecursiva( double x) {		///2
 		int aux = 0;
-		for (int i = 0; i < this.grado; i++) {
-			aux += coeficientes[i] * Polinomio.powRec(x, this.grado - i -1);
+		for (int i = 0; i <= this.grado; i++) {
+			aux += coeficientes[i] * powRec(x, this.grado - i );
 		}
 		return aux;
 	}
 	
-	public double evaluarProgDinamica(double x) {
+	public static double evaluarIndiceParapow(double x,int i) {		///2
+		return i % 2 == 0 ? powRec(x * x, i / 2) : powRec(x, i);
+	}
+	
+	public static double powRec( double x, int i) {
+		if (i == 1) 
+			return x;
+		if (i == 0) 
+			return 1;
+		return (x * powRec(x, i - 1));
+	}
+	
+	public static double powMia(double x, int i) {
+		int aux = 1;
+		for (int j = 0; j < i; j++) {
+			aux *= x;
+		}
+		return aux;
+	}
+	
+	public double evaluarProgDinamica(double x) {		///3
 		double sumador=0;
 		double potencias_dinamicas[] = new double[this.grado+1];
 		int aux=this.grado;
@@ -43,28 +82,31 @@ public class Polinomio {
 		return sumador;
 	}
 	
-	public static double evaluarIndiceParapow(double x,int i) {
-		return i % 2 == 0 ? powRec(x * x, i / 2) : powRec(x, i);
-	}
-	
-	public static double powRec( double x, int i) {
-		if (i == 1) {
-			return x;
-		} else if (i == 0) {
-			return 1;
+	public double evaluarMejorada(double x) {		///4
+		double sumador=0;
+		double potencias_dinamicas_mejoradas[] = new double[this.grado+1];
+		
+		potencias_dinamicas_mejoradas[0]=1;
+		potencias_dinamicas_mejoradas[1]=x;
+		
+		for (int i = 0; i <=this.grado; i++) {
+			if(i>1)
+				potencias_dinamicas_mejoradas[i]= potencias_dinamicas_mejoradas[i-1]*x;
+			sumador+=this.coeficientes[this.grado-i]*potencias_dinamicas_mejoradas[i];
 		}
-		return (x * powRec(x, i - 1));
+		
+		return sumador;
 	}
 	
-	public static double powMia(double x, int i) {
-		int aux = 1;
-		for (int j = 0; j < i; j++) {
-			aux *= x;
+	public double evaluarPow(double x) {		///5
+		int aux = 0;
+		for (int i = 0; i <= this.grado; i++) {
+			aux += coeficientes[i] * Math.pow(x, this.grado - i );
 		}
 		return aux;
 	}
 	
-	public double evaluarHorner(double x) {
+	public double evaluarHorner(double x) {		///6
 	        double result = coeficientes[0];   
 	   
 	        for (int i=1; i<coeficientes.length; i++) 
@@ -76,13 +118,7 @@ public class Polinomio {
 	
 	
 	
-	public double evaluarRecursiva( double x) {
-		int aux = 0;
-		for (int i = 0; i < this.grado; i++){
-			aux += coeficientes[i] * (x * powRec(x, this.grado - i - 2));
-		}
-		return aux;
-	}
+	
 
 	public int getGrado() {
 		return grado;
